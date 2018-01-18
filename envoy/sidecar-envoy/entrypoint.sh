099aef965f62bc60cc8af54cb3d94623ed5799a4
senlinzhan <senlinzhan@gmail.com>
2018-01-18 18:11:07 +0800

    add prometheus

--- envoy/sidecar-envoy/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python app.py &
-/usr/local/bin/envoy -c /etc/envoy.json
-
--- prometheus/docker/app.py
+++ prometheus/docker/app.py
@@ -71,4 +71,4 @@ def metrics():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
